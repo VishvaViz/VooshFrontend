79c0f1ec322893451599d5581182ca021a519253
VishvaZen <vishva.p@mindstaytechnology.in>
2024-07-26 01:06:05 +0530

    route

--- src/route/Route.jsx
+++ src/route/Route.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from "react";
+import PrivateRoute from "./PrivateRoute";
 
-
-const Signup=lazy(()=>import('../components/login/Signup'))
-const Login=lazy(()=>import('../components/login/Login'))
-const Home=lazy(()=>import('../components/page/Home'))
+const Signup = lazy(() => import('../components/login/Signup'))
+const Login = lazy(() => import('../components/login/Login'))
+const Home = lazy(() => import('../components/page/Home'))
 
 
 
@@ -16,10 +16,13 @@ function Routecomp() {
                 <Suspense
                     fallback={<div>Loading...</div>}
                 >
+
                     <Routes>
+                        <Route element={<PrivateRoute/>} >
+                            <Route path="/home" element={<Home />} />
+                        </Route>
                         <Route path="/" element={<Signup />} />
                         <Route path="/login" element={<Login />} />
-                        <Route path="/home" element={<Home/>}/>
                     </Routes>
                 </Suspense>
             </BrowserRouter>
